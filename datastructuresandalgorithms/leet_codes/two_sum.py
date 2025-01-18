ca04aca1cf5a1260a3b8672da416c4c3563be4ce
elias-maciel <eliasfsilvamaciel@gmail.com>
2025-01-17 22:38:46 -0300

    add hash solution

--- datastructuresandalgorithms/leet_codes/two_sum.py
+++ datastructuresandalgorithms/leet_codes/two_sum.py
@@ -3,10 +3,17 @@ from typing import List
 
 class Solution:
     def bad_solution(self, nums: List[int], target: int) -> List[int]:
-        for idx, num1 in enumerate(nums):
-            for num2 in nums[idx:]:
+        for idx1, num1 in enumerate(nums):
+            for idx2, num2 in enumerate(nums[idx1:]):
                 if num1 + num2 == target:
-                    return [num1, num2]
+                    return [idx1, idx2]
+
+    def two_sum(self, nums: List[int], target: int) -> List[int]:
+        hasher = {}
+        for idx, i in enumerate(nums):
+            if hasher.get(i) is not None:
+                return [hasher.get(i), idx]
+            hasher[target - i] = idx
 
 
 if __name__ == '__main__':
